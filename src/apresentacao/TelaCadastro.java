91635cbccc0c3288774d287f665c5ef7304c7211
fsdenzelb <200688654+fsdenzelb@users.noreply.github.com>
2025-08-21 23:26:13 -0300

    Adicionando comentários importantes

--- src/apresentacao/TelaCadastro.java
+++ src/apresentacao/TelaCadastro.java
@@ -17,16 +17,28 @@ public class TelaCadastro extends JFrame {
     }
 
     private void construirTela() {
-        setSize(400, 400);
         setTitle("Cadastro de Clientes");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+
         this.adicionarCampos();
+        this.adicionarBotoes();
+
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
     }
 
     private void adicionarCampos() {
         JPanel form = new JPanel(new GridLayout(0, 2, 8, 8));
+        //JPanel é como um contâiner utilizado para organizar os elementos dentro de interface. Essa nova instância de JPanel está criando, por consequência, um GridLayout, que organiza os elementos dentro do painel.
+        /*Rows: 0 -> O layout vai calcular automaticamente o número de linhas, baseado na quantidade de componentes adicionados e no número de colunas. Aqui está definido como 0, ou seja, as linhas vão crescer conforme você adiciona mais componentes.
+        Cols: 2 -> 2 elementos por coluna.
+        HGap: 8 -> Espaçamento horizontal (em pixels) entre as colunas.
+        VGap: 8 -> Espaçamento vertical (em pixels entre as linhas.*/
+
         form.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+        //Apenas adicionando uma borda nas 4 direções.
 
         campoNome = new JTextField(20);
         campoCnpj = new JTextField(20);
@@ -40,16 +52,17 @@ public class TelaCadastro extends JFrame {
         form.add(new JLabel("Endereco:")); form.add(campoEndereco);
         form.add(new JLabel("Telefone:")); form.add(campoTelefone);
 
+        add(form, BorderLayout.CENTER);
+    }
+
+    private void adicionarBotoes() {
         botaoCadastrar = new JButton("Cadastrar");
-        JPanel botoes = new JPanel(new FlowLayout(FlowLayout.RIGHT, 8, 8));
+        JPanel botoes = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 8));
         botoes.add(botaoCadastrar);
 
-        add(form, BorderLayout.CENTER);
-        add(botoes, BorderLayout.SOUTH);
-
         getRootPane().setDefaultButton(botaoCadastrar);
-        pack();                   // calcula tamanho ideal
-        setLocationRelativeTo(null); // centraliza
+        //Adiciona o ENTER como um atalho para o botãoCadastrar.
 
+        add(botoes, BorderLayout.SOUTH);
     }
 }
