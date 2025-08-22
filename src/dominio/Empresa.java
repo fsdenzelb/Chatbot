4508e983ec0ed1966ff7e36ceccd714880a878f5
fsdenzelb <200688654+fsdenzelb@users.noreply.github.com>
2025-08-22 00:12:36 -0300

    Adicionando classe Empresa para cadastro

---
+++ src/dominio/Empresa.java
@@ -0,0 +1,57 @@
+package dominio;
+
+public class Empresa {
+    private String nome;
+    private String cnpj;
+    private String email;
+    private String endereco;
+    private String telefone;
+
+    public Empresa(String nome, String cnpj, String email, String endereco, String telefone) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
